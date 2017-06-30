6f46d520569674f4b42f34627b57d15b02fb460f
Michael Granger <ged@FaerieMUD.org>
2017-06-30 14:27:56 -0700

    Initial commit.

---
+++ spec/arborist/monitor/dns_spec.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env rspec -cfd
+#encoding: utf-8
+
+require_relative '../../spec_helper'
+
+require 'rspec'
+require 'arborist/monitor/dns'
+
+describe Arborist::Monitor::Dns do
+
+	
+
+end
+
